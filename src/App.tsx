55ed98458fd32905010cb2883e7e172d08acdc1c
Nam Nguyen <nam.nguyen@shopify.com>
2026-02-27 18:13:40 +0700

    🚦

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { signal, useModel } from "@preact/signals-react";
+import { createModel } from "@preact/signals-core";
 
-function App() {
-  const [count, setCount] = useState(0)
+const countModel = createModel(() => {
+  const count = signal(0);
+  const increment = () => {
+    count.value++;
+  };
+  return {
+    count,
+    increment,
+  };
+});
 
+export default function App() {
+  console.log("rendering app");
+  const { count, increment } = useModel(countModel);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <button onClick={increment}>Increment</button>
+      <p>Value: {count}</p>
+    </div>
+  );
 }
-
-export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import Inspect from "vite-plugin-inspect";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    Inspect(),
+    react({
+      babel: {
+        plugins: [["module:@preact/signals-react-transform"]],
+      },
+    }),
+  ],
+});
