74ee845f768944208d75f358877f2ddf136aec25
Nam Nguyen <nam.nguyen@shopify.com>
2026-03-03 09:22:58 +0700

    📡

--- src/model.ts
+++ src/model.ts
@@ -2,9 +2,11 @@ import { signal, computed, createModel, effect } from "@preact/signals-core";
 
 export const AppModel = createModel(() => {
   console.log("creating app model");
-  const firstNumber = signal(0);
-  const secondNumber = signal(0);
-  const sum = computed(() => firstNumber.value + secondNumber.value);
+  const firstNumber = signal(0, { name: "firstNumber" });
+  const secondNumber = signal(0, { name: "secondNumber" });
+  const sum = computed(() => firstNumber.value + secondNumber.value, {
+    name: "sum",
+  });
 
   const setFirstNumber = (value: number) => {
     firstNumber.value = value;
